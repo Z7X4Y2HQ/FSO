5410bf37b62bc3a4dc4c2ba6740db071ba72fa63
Z7X4Y2HQ <87140889+Z7X4Y2HQ@users.noreply.github.com>
2022-10-25 01:39:51 +0500

    fucking finally

--- part2/Getting data from server/countries/src/components/Display.js
+++ part2/Getting data from server/countries/src/components/Display.js
@@ -1,46 +1,87 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const Info = ({ filter }) => {
-  return filter.map((e) => (
-    <div key={e.flag}>
-      <h1>{e.name.common}</h1>
-      <p>
-        {e.capital} <br /> {e.area}
-      </p>
-      <h3>Languages</h3>
-      <ul>
-        {Object.values(e.languages).map((e) => (
-          <li key={e}>{e}</li>
-        ))}
-      </ul>
-      <img style={{ width: 200 }} src={e.flags.png}></img>
-    </div>
-  ));
+const SingleInfo = ({ data }) => {
+  const [weather, setWeather] = useState([]);
+
+  return data.map((e) => {
+    return (
+      <div key={e.flag}>
+        <h1>{e.name.common}</h1>
+        <p>
+          capital {e.capital} <br /> area {e.area}
+        </p>
+        <h3>Languages</h3>
+        <ul>
+          {Object.values(e.languages).map((e) => (
+            <li key={e}>{e}</li>
+          ))}
+        </ul>
+        <img style={{ width: 200 }} src={e.flags.png}></img> <br />
+      </div>
+    );
+  });
+};
+
+const Info = ({ data }) => {
+  const [show, setShow] = useState(true);
+  const [clicked, setClicked] = useState();
+
+  return (
+    <>
+      {data.map((e) => {
+        return (
+          <div key={e.flag}>
+            {e.name.common}{" "}
+            <button
+              onClick={() => {
+                setShow(false);
+                setClicked(e.name.common);
+              }}
+            >
+              Show
+            </button>
+          </div>
+        );
+      })}
+      {!show &&
+        data
+          .filter((e) => {
+            if (e.name.common == clicked) {
+              return e;
+            }
+          })
+          .map((e) => {
+            return (
+              <div key={e.flag}>
+                <h1>{e.name.common}</h1>
+                <p>
+                  capital {e.capital} <br /> area {e.area}
+                </p>
+                <h3>Languages</h3>
+                <ul>
+                  {Object.values(e.languages).map((e) => (
+                    <li key={e}>{e}</li>
+                  ))}
+                </ul>
+                <img style={{ width: 200 }} src={e.flags.png}></img> <br />
+              </div>
+            );
+          })}
+    </>
+  );
 };
 
 const Display = ({ countries, search }) => {
-  let filter = countries.filter((e) => e.name.common.toUpperCase().includes(search.toUpperCase()));
+  let data = countries.filter((e) => e.name.common.toUpperCase().includes(search.toUpperCase()));
+
   const [show, setShow] = useState(true);
-  console.log(show);
+
   const func = () => {
-    if (filter.length == 1) {
-      return <Info filter={filter} />;
-    } else if (filter.length < 10) {
-      return filter.map((e) => (
-        <>
-          {show && (
-            <div key={e.flag}>
-              {e.name.common} <button onClick={() => setShow(false)}>Show</button>
-            </div>
-          )}
-          {!show && (
-            <>
-              <button onClick={() => setShow(true)}>Hide</button>
-              <Info filter={filter} />
-            </>
-          )}
-        </>
-      ));
+    if (data.length == 1) {
+      return <SingleInfo data={data} />;
+    } else if (data.length < 10) {
+      return <Info data={data} />;
     } else {
       return <div>Too many matches, specify another filter</div>;
     }
