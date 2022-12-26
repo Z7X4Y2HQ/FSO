acee27d6587ba8fce3da7b1ff939cda1bbbf7c5d
Z7X4Y2HQ <87140889+Z7X4Y2HQ@users.noreply.github.com>
2022-12-27 01:13:21 +0500

    wow

--- part3/Node.js and Express/index.js
+++ part3/Node.js and Express/index.js
@@ -1,5 +1,8 @@
+const { request, response } = require("express");
 const express = require("express");
 const app = express();
+
+app.use(express.json());
 
 let notes = [
   {
@@ -22,6 +25,11 @@ let notes = [
   },
 ];
 
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
 app.get("/", (request, response) => {
   response.send("<h1>Hello World</h1>");
 });
@@ -38,6 +46,33 @@ app.get("/api/notes/:id", (request, response) => {
   } else {
     response.status(404).end();
   }
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+  response.status(204).end();
+});
+
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  };
+
+  notes = note.concat(note);
+
+  response.json(note);
 });
 
 const PORT = 3001;
