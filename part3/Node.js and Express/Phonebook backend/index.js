73e8a38e4ec0a3703bcf94e15aca89257ac263dc
Z7X4Y2HQ <87140889+Z7X4Y2HQ@users.noreply.github.com>
2023-01-03 15:15:57 +0500

    ...

--- part3/Node.js and Express/Phonebook backend/index.js
+++ part3/Node.js and Express/Phonebook backend/index.js
@@ -45,5 +45,11 @@ app.get("/api/persons/:id", (req, res) => {
   }
 });
 
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.filter((person) => person.id !== id);
+  res.status(204).end();
+});
+
 const port = 3001;
 app.listen(port);
